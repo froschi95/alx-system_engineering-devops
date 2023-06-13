0261d91c975d4d53301db6d74e6e2a66ef22eaff
Frank Success <60326840+froschi95@users.noreply.github.com>
2023-06-13 13:29:59 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,14 @@ Contains the number_of_subscribers function
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Python/requests:APIproject:\v1.0.0 (by /u/aaorrico23)'}
+    headers = {
+        'User-Agent': 'Python/requests:APIproject:\v1.0.0 (by /u/aaorrico23)'
+    }
     
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
