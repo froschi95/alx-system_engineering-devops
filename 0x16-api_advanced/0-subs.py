e2a6b73d6acd8672ff236ff70ed9e20bdb0a0315
Frank Success <60326840+froschi95@users.noreply.github.com>
2023-06-13 13:35:03 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,6 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'Python/requests:APIproject:\v1.0.0 (by /u/aaorrico23)'
     }
-    
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
